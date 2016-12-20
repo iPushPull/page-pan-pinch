c048f18f9d962cfab187b4da63bc8a21f46d1630
Kyle Hengst <kyle.hengst@ipushpull.com>
2016-12-20 18:04:10 +0000

    updates

--- page-pan-pinch.js
+++ page-pan-pinch.js
@@ -3,8 +3,11 @@ var PagePanPinch = (function () {
         var _this = this;
         this.options = {
             bounds: "",
-            content: "",
+            contentScroll: "",
+            contentZoom: "",
             page: "",
+            debug: "",
+            onTap: function (pt, ev) { },
             onDoubleTap: function (pt, ev) { }
         };
         this._scale = {
@@ -14,14 +17,22 @@ var PagePanPinch = (function () {
             max: 2
         };
         this._pos = {
+            friction: .9,
             x: {
                 last: 0,
-                current: 0
+                current: 0,
+                vel: 0,
+                delta: 0
             },
             y: {
                 last: 0,
-                current: 0
+                current: 0,
+                vel: 0,
+                delta: 0
             }
+        };
+        this._eventPinchStart = function (ev) {
+            _this.clearPanTimer();
         };
         this._eventPinchMove = function (ev) {
             _this._scale.current = _this._scale.last * ev.scale;
@@ -39,23 +50,35 @@ var PagePanPinch = (function () {
             _this.update();
         };
         this._eventTap = function (ev) {
-            if (ev.tapCount === 2) {
+            if (ev.tapCount === 1) {
+                _this.options.onTap(_this, ev);
+            }
+            else if (ev.tapCount === 2) {
                 _this.options.onDoubleTap(_this, ev);
             }
             return;
         };
         this._eventPanStart = function (ev) {
-            console.log("pan start");
+            _this.clearPanTimer();
         };
         this._eventPanMove = function (ev) {
-            _this._pos.x.current = _this._pos.x.last + (ev.deltaX * 1 / _this._scale.last);
-            _this._pos.y.current = _this._pos.y.last + (ev.deltaY * 1 / _this._scale.last);
+            _this._pos.x.current = _this._pos.x.last + (ev.deltaX);
+            _this._pos.y.current = _this._pos.y.last + (ev.deltaY);
             _this.setWithinBounds();
             _this.update();
         };
         this._eventPanEnd = function (ev) {
             _this._pos.x.last = _this._pos.x.current;
             _this._pos.y.last = _this._pos.y.current;
+            _this._pos.x.vel = ev.velocityX;
+            _this._pos.y.vel = ev.velocityY;
+            _this._pos.x.delta = ev.deltaX;
+            _this._pos.y.delta = ev.deltaY;
+            if (Math.abs(ev.velocity) > .2) {
+                _this._panTimer = setInterval(function () {
+                    _this.updatePanVel();
+                }, 20);
+            }
         };
         if (window.Hammer === undefined) {
             throw "Hammer JS not found";
@@ -66,46 +89,85 @@ var PagePanPinch = (function () {
             }
         }
         this._bounds = document.getElementById(this.options.bounds);
-        this._content = document.getElementById(this.options.content);
+        this._contentScroll = document.getElementById(this.options.contentScroll);
+        this._contentZoom = document.getElementById(this.options.contentZoom);
         this._page = document.getElementById(this.options.page);
-        if (!this._bounds || !this._content || !this._page) {
+        this._debug = document.getElementById(this.options.debug);
+        if (!this._bounds || !this._contentScroll || !this._contentZoom || !this._page) {
             throw "DOM Elements undefined";
         }
         this._boundsRect = this._bounds.getBoundingClientRect();
         this._pageRect = this._page.getBoundingClientRect();
+        this._bounds.style.overflow = "hidden";
+        this._contentScroll.style.transformOrigin = "0 0";
+        this._contentScroll.style.width = this._pageRect.width + "px";
+        this._contentScroll.style.height = this._pageRect.height + "px";
+        this._contentZoom.style.transformOrigin = "50% 50%";
+        this.setupTouch();
+        this.init(true);
+    }
+    PagePanPinch.prototype.init = function (scale) {
+        this.setMaxMinScale(scale);
+        this.setWithinBounds();
+        this.update();
+    };
+    PagePanPinch.prototype.refresh = function () {
+        this._boundsRect = this._bounds.getBoundingClientRect();
+        this.setupTouch();
+        this.init(false);
+    };
+    PagePanPinch.prototype.setupTouch = function () {
+        if (this._mc) {
+            this._mc.destroy();
+        }
         this._mc = new Hammer(this._bounds);
+        var pan = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL });
         var pinch = new Hammer.Pinch();
         var tap = new Hammer.Tap({ event: "singletap" });
         var doubleTap = new Hammer.Tap({ event: "doubletap", taps: 2 });
         doubleTap.recognizeWith(tap);
-        this._mc.add([pinch, doubleTap]);
+        this._mc.add([pan, pinch, doubleTap]);
+        this._mc.on("pinchstart", this._eventPinchStart);
         this._mc.on("pinchmove", this._eventPinchMove);
-        this._mc.on("pinchend", function (ev) {
-            _this._eventPinchEnd(ev);
-        });
-        this._mc.on("tap", function (ev) {
-            _this._eventTap(ev);
-        });
-        this._mc.on("panstart", function (ev) {
-            _this._eventPanStart(ev);
-        });
-        this._mc.on("panmove", function (ev) {
-            _this._eventPanMove(ev);
-        });
-        this._mc.on("panend", function (ev) {
-            _this._eventPanEnd(ev);
-        });
-        this.init();
-    }
-    PagePanPinch.prototype.init = function () {
-        this.setMaxMinScale();
+        this._mc.on("pinchend", this._eventPinchEnd);
+        this._mc.on("tap", this._eventTap);
+        this._mc.on("panstart", this._eventPanStart);
+        this._mc.on("panmove", this._eventPanMove);
+        this._mc.on("panend", this._eventPanEnd);
+    };
+    PagePanPinch.prototype.clearPanTimer = function () {
+        if (this._panTimer) {
+            clearInterval(this._panTimer);
+        }
+    };
+    PagePanPinch.prototype.updatePanVel = function () {
+        this._pos.x.current += this._pos.x.delta;
+        this._pos.y.current += this._pos.y.delta;
+        this._pos.x.last = this._pos.x.current;
+        this._pos.y.last = this._pos.y.current;
+        this._pos.x.delta *= this._pos.friction;
+        this._pos.y.delta *= this._pos.friction;
+        this.setWithinBounds();
         this.update();
+        if (Math.abs(this._pos.x.delta) < .01 && Math.abs(this._pos.y.delta) < .01) {
+            clearInterval(this._panTimer);
+        }
     };
     PagePanPinch.prototype.update = function () {
-        this._content.style.transform = "translateZ(0px) scale(" + (this._scale.current) + ") translate(" + Math.round(this._pos.x.current) + "px," + Math.round(this._pos.y.current) + "px)";
+        var width = Math.round(this._pageRect.width * this._scale.last);
+        var height = Math.round(this._pageRect.height * this._scale.last);
+        var x = Math.round(this._pos.x.current);
+        var y = Math.round(this._pos.y.current);
+        this._contentScroll.style.transform = "translateZ(0px) translate(" + x + "px, " + y + "px)";
+        this._contentZoom.style.transform = "translateZ(0px) scale(" + this._scale.current + ")";
+        if (this._debug) {
+            this._debug.innerHTML = "x: " + x + ", y: " + y + ", width: " + width + ", height: " + height + ", scale: " + this._scale.last;
+        }
     };
-    PagePanPinch.prototype.setMaxMinScale = function () {
-        this._scale.last = this._scale.current = this._boundsRect.width / this._pageRect.width;
+    PagePanPinch.prototype.setMaxMinScale = function (scale) {
+        if (scale) {
+            this._scale.last = this._scale.current = this._boundsRect.width / this._pageRect.width;
+        }
         var hScale = this._boundsRect.height / this._pageRect.height;
         this._scale.min = hScale < this._scale.last ? hScale : this._scale.current;
         var vScale = this._boundsRect.width / this._pageRect.width;
@@ -116,29 +178,31 @@ var PagePanPinch = (function () {
     PagePanPinch.prototype.setWithinBounds = function () {
         var pageWidth = this._pageRect.width * this._scale.current;
         var pageHeight = this._pageRect.height * this._scale.current;
-        var maxLeft = (pageWidth - this._boundsRect.width) * 1 / this._scale.current;
-        var maxTop = (pageHeight - this._boundsRect.height) * 1 / this._scale.current;
+        var maxLeft = (pageWidth - this._pageRect.width) / 2 + (this._pageRect.width - this._boundsRect.width);
+        var maxTop = (pageHeight - this._pageRect.height) / 2 + (this._pageRect.height - this._boundsRect.height);
+        var minLeft = (pageWidth - this._pageRect.width) / 2;
+        var minTop = (pageHeight - this._pageRect.height) / 2;
         if (pageWidth > this._boundsRect.width) {
             if (this._pos.x.current < -maxLeft) {
                 this._pos.x.current = -maxLeft;
             }
-            else if (this._pos.x.current >= 0) {
-                this._pos.x.current = 0;
+            else if (this._pos.x.current >= minLeft) {
+                this._pos.x.current = minLeft;
             }
         }
         else {
-            this._pos.x.current = 0;
+            this._pos.x.current = minLeft;
         }
         if (pageHeight > this._boundsRect.height) {
             if (this._pos.y.current < -maxTop) {
                 this._pos.y.current = -maxTop;
             }
-            else if (pageHeight < this._boundsRect.height || this._pos.y.current >= 0) {
-                this._pos.y.current = 0;
+            else if (this._pos.y.current >= minTop) {
+                this._pos.y.current = minTop;
             }
         }
         else {
-            this._pos.y.current = 0;
+            this._pos.y.current = minTop;
         }
     };
     return PagePanPinch;
--- page-pan-pinch.ts
+++ page-pan-pinch.ts
@@ -2,16 +2,21 @@ class PagePanPinch {
 
     public options: any = {
         bounds: "",
-        content: "",
+        contentScroll: "",
+        contentZoom: "",
         page: "",
+        debug: "",
+        onTap: (pt, ev) => { },
         onDoubleTap: (pt, ev) => { }
     };
 
     private _bounds: any;
     private _boundsRect: any;
-    private _content: any;
+    private _contentScroll: any;
+    private _contentZoom: any;
     private _page: any;
     private _pageRect: any;
+    private _debug: any;
 
     private _scale: any = {
         last: 1,
@@ -21,15 +26,22 @@ class PagePanPinch {
     };
 
     private _pos: any = {
+        friction: .9,
         x: {
             last: 0,
-            current: 0
+            current: 0,
+            vel: 0,
+            delta: 0
         },
         y: {
             last: 0,
-            current: 0
+            current: 0,
+            vel: 0,
+            delta: 0
         }
     };
+
+    private _panTimer: any;
 
     // Hammer Libary
     private _mc: any;
@@ -49,48 +61,69 @@ class PagePanPinch {
 
         // dom elements
         this._bounds = document.getElementById(this.options.bounds);
-        this._content = document.getElementById(this.options.content);
+        this._contentScroll = document.getElementById(this.options.contentScroll);
+        this._contentZoom = document.getElementById(this.options.contentZoom);
         this._page = document.getElementById(this.options.page);
+        this._debug = document.getElementById(this.options.debug);
 
-        if (!this._bounds || !this._content || !this._page) {
+        if (!this._bounds || !this._contentScroll || !this._contentZoom || !this._page) {
             throw "DOM Elements undefined";
         }
 
         this._boundsRect = this._bounds.getBoundingClientRect();
         this._pageRect = this._page.getBoundingClientRect();
+        this._bounds.style.overflow = "hidden";
+        this._contentScroll.style.transformOrigin = "0 0";
+        this._contentScroll.style.width = `${this._pageRect.width}px`;
+        this._contentScroll.style.height = `${this._pageRect.height}px`;
+        this._contentZoom.style.transformOrigin = "50% 50%";
 
+        this.setupTouch();
+
+        this.init(true);
+
+    }
+
+    public init(scale: boolean): void {
+        this.setMaxMinScale(scale);
+        this.setWithinBounds();
+        this.update();
+    }
+
+    public refresh(): void {
+        this._boundsRect = this._bounds.getBoundingClientRect();
+        this.setupTouch();
+        this.init(false);
+    }
+
+    private setupTouch(): void {
+        if (this._mc) {
+            this._mc.destroy();
+        }
         // touch library
         this._mc = new Hammer(this._bounds);
-        // let pan: any = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL });
+        let pan: any = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL });
         let pinch: any = new Hammer.Pinch();
         let tap: any = new Hammer.Tap({ event: "singletap" });
         let doubleTap: any = new Hammer.Tap({ event: "doubletap", taps: 2 });
         doubleTap.recognizeWith(tap);
 
         // add events
-        this._mc.add([pinch, doubleTap]);
+        this._mc.add([pan, pinch, doubleTap]);
 
         // event listeners
+        this._mc.on("pinchstart", this._eventPinchStart);
         this._mc.on("pinchmove", this._eventPinchMove);
-        this._mc.on("pinchend", (ev) => {
-            this._eventPinchEnd(ev);
-        });
-        this._mc.on("tap", (ev) => {
-            this._eventTap(ev);
-        });
-        this._mc.on("panstart", (ev) => {
-            this._eventPanStart(ev);
-        });
-        this._mc.on("panmove", (ev) => {
-            this._eventPanMove(ev);
-        });
-        this._mc.on("panend", (ev) => {
-            this._eventPanEnd(ev);
-        });
-
-        this.init();
-
+        this._mc.on("pinchend", this._eventPinchEnd);
+        this._mc.on("tap", this._eventTap);
+        this._mc.on("panstart", this._eventPanStart);
+        this._mc.on("panmove", this._eventPanMove);
+        this._mc.on("panend", this._eventPanEnd);        
     }
+
+    private _eventPinchStart = (ev: any): void => {
+        this.clearPanTimer();
+    };
 
     private _eventPinchMove = (ev: any): void => {
         this._scale.current = this._scale.last * ev.scale;
@@ -110,44 +143,83 @@ class PagePanPinch {
     };
 
     private _eventTap = (ev: any): void => {
-        if (ev.tapCount === 2) {
+        if (ev.tapCount === 1) {
+            this.options.onTap(this, ev);
+        } else if (ev.tapCount === 2) {
             this.options.onDoubleTap(this, ev);
         }
         return;
     };
 
     private _eventPanStart = (ev: any): void => {
-        console.log("pan start");
+        this.clearPanTimer();
     };
 
     private _eventPanMove = (ev: any): void => {
 
-        this._pos.x.current = this._pos.x.last + (ev.deltaX * 1 / this._scale.last);
-        this._pos.y.current = this._pos.y.last + (ev.deltaY * 1 / this._scale.last);
+        this._pos.x.current = this._pos.x.last + (ev.deltaX);
+        this._pos.y.current = this._pos.y.last + (ev.deltaY);
 
         this.setWithinBounds();
         this.update();
     };
 
     private _eventPanEnd = (ev: any): void => {
-
         this._pos.x.last = this._pos.x.current;
         this._pos.y.last = this._pos.y.current;
-
+        this._pos.x.vel = ev.velocityX;
+        this._pos.y.vel = ev.velocityY;
+        this._pos.x.delta = ev.deltaX;
+        this._pos.y.delta = ev.deltaY;
+        if (Math.abs(ev.velocity) > .2) {
+            this._panTimer = setInterval(() => {
+                this.updatePanVel();
+            }, 20);
+        }
     };
 
-    private init(): any {
-        this.setMaxMinScale();
+    private clearPanTimer(): void {
+        if (this._panTimer) {
+            clearInterval(this._panTimer);
+        }
+    }
+
+    private updatePanVel(): void {
+        this._pos.x.current += this._pos.x.delta;
+        this._pos.y.current += this._pos.y.delta;
+        this._pos.x.last = this._pos.x.current;
+        this._pos.y.last = this._pos.y.current;
+        this._pos.x.delta *= this._pos.friction;
+        this._pos.y.delta *= this._pos.friction;
+        this.setWithinBounds();
         this.update();
+        if (Math.abs(this._pos.x.delta) < .01 && Math.abs(this._pos.y.delta) < .01) {
+            clearInterval(this._panTimer);
+        }
+
     }
 
     private update(): any {
-        this._content.style.transform = "translateZ(0px) scale(" + (this._scale.current) + ") translate(" + Math.round(this._pos.x.current) + "px," + Math.round(this._pos.y.current) + "px)";
+
+        let width: number = Math.round(this._pageRect.width * this._scale.last);
+        let height: number = Math.round(this._pageRect.height * this._scale.last);
+
+        let x: number = Math.round(this._pos.x.current);
+        let y: number = Math.round(this._pos.y.current);
+
+        this._contentScroll.style.transform = `translateZ(0px) translate(${x}px, ${y}px)`;
+        this._contentZoom.style.transform = `translateZ(0px) scale(${this._scale.current})`;
+
+        if (this._debug) {
+            this._debug.innerHTML = `x: ${x}, y: ${y}, width: ${width}, height: ${height}, scale: ${this._scale.last}`;
+        }
     }
 
-    private setMaxMinScale(): any {
+    private setMaxMinScale(scale: boolean): any {
 
-        this._scale.last = this._scale.current = this._boundsRect.width / this._pageRect.width;
+        if (scale) {
+            this._scale.last = this._scale.current = this._boundsRect.width / this._pageRect.width;
+        }
 
         let hScale: number = this._boundsRect.height / this._pageRect.height;
         this._scale.min = hScale < this._scale.last ? hScale : this._scale.current;
@@ -164,30 +236,34 @@ class PagePanPinch {
         let pageWidth: number = this._pageRect.width * this._scale.current;
         let pageHeight: number = this._pageRect.height * this._scale.current;
 
-        let maxLeft: number = (pageWidth - this._boundsRect.width) * 1 / this._scale.current;
-        let maxTop: number = (pageHeight - this._boundsRect.height) * 1 / this._scale.current;
+        let maxLeft: number = (pageWidth - this._pageRect.width) / 2 + (this._pageRect.width - this._boundsRect.width);
+        let maxTop: number = (pageHeight - this._pageRect.height) / 2 + (this._pageRect.height - this._boundsRect.height);
+
+        let minLeft: number = (pageWidth - this._pageRect.width) / 2;
+        let minTop: number = (pageHeight - this._pageRect.height) / 2;
 
         if (pageWidth > this._boundsRect.width) {
             if (this._pos.x.current < -maxLeft) {
                 this._pos.x.current = -maxLeft;
-            } else if (this._pos.x.current >= 0) {
-                this._pos.x.current = 0;
+            } else if (this._pos.x.current >= minLeft) {
+                this._pos.x.current = minLeft;
             }
         } else {
-            this._pos.x.current = 0;
+            this._pos.x.current = minLeft;
         }
 
         if (pageHeight > this._boundsRect.height) {
             if (this._pos.y.current < -maxTop) {
                 this._pos.y.current = -maxTop;
-            } else if (pageHeight < this._boundsRect.height || this._pos.y.current >= 0) {
-                this._pos.y.current = 0;
+            } else if (this._pos.y.current >= minTop) {
+                this._pos.y.current = minTop;
             }
         } else {
-            this._pos.y.current = 0;
+            this._pos.y.current = minTop;
         }
 
     }
+
 
 }
 
